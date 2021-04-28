a2541d44a5d95814ebcc48dabe81d42a3c525efc
Sergio <gonzalez.sergiod@gmail.com>
2021-04-27 23:49:31 -0300

    prueba..........

--- src/main/java/com/tea/springboot/HELPERS/FechaHelper.java
+++ src/main/java/com/tea/springboot/HELPERS/FechaHelper.java
@@ -6,12 +6,10 @@ import java.text.SimpleDateFormat;
 public class FechaHelper {
 
 	public static String convertirFechaAFormatoJapones(Date fecha) {
-		
 		return new SimpleDateFormat("yyyyMMdd").format(fecha);
 	}
 	
 	public static String convertirFechaAFormatoddMMyyyy(String fecha) {
-		
 		return fecha.substring(6,8) + "/" + fecha.substring(4,6) + "/" + fecha.substring(0,4) ;
 	}
 }
